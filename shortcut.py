bb61f1a49acc4420bcf2ad73f036a5d10cfe789b
root <root@Kaseys-Laptop.localdomain>
2023-10-26 16:57:41 -0400

    update

---
+++ shortcut.py
@@ -0,0 +1,102 @@
+#!/usr/bin/python3 
+
+"""
+Kasey Kiggins
+Kak8381@rit.edu
+Date: 10/25/2023
+"""
+
+#add imports
+import shutil
+import subprocess
+import platform
+import re
+import os
+
+
+#must create links on the users desktop "/home/user/desktop"
+#make symbolic link, delete a link, and run report
+
+#command line menu
+def menu():
+    directory = os.popen("cd").read()
+    print("You are currently in " + directory +
+        "\nPlease pick an option:" + 
+        "\n1.) Make symbolic link" +
+        "\n2.) Delete a link" +
+        "\n3.) Run Report" +
+        "\ntype quit to end")
+
+
+
+def makelink(filename):
+    if os.path.exists(filename):
+        print("Creating Link...")
+        #make link
+        src = os.path.basename(filename)
+        dst = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop')
+        os.symlink(src, dst)
+        print("Link successful!!")
+    else:
+        print("Oh no! Could not find file, please make sure file exists.")
+
+def deletelink(linkname):
+    if os.path.exists(linkname):
+        #delete link
+        if os.path.isdir(linkname):
+            if os.path.islink(linkname):
+                os.unlink(linkname)
+            else:
+                shutil.rmtree(linkname)
+                print("Link successfully deleted!!")
+        else:
+            if os.path.islink(linkname):
+                os.unlink(linkname)
+            else:
+                removeobj(linkname)
+            
+    else:
+        print("Oh no! Could not find link or file, please make sure link exists.")
+
+def removeobj(linkname):
+    var = input("Link was not found would you still like to remove this object?(yes/no)")
+    if var.islower() == "yes":
+        os.remove(linkname)
+        print("object deleted")
+    elif var.islower() == "no":
+        return 0
+    else:
+        print("please enter yes or no")
+        removeobj(linkname)
+
+def report():
+    dirname = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop')
+    linklist = os.popen("find " + dirname + " -type l").read().split("\n")
+    print("The links you have in " + dirname + " are:")
+    for i in range(0,len(linklist)):
+        print(i + "\n")
+
+def main():
+    menu()
+    print("Enter you selection: ")
+    val = input("")
+    if val == "1":
+        filename = input("Please enter a filename to make a link with:\n ")
+        makelink(filename)
+    elif val == "2":
+        linkname = input("Please enter a link you would like to delete:\n ")
+        deletelink(linkname)
+    elif val == "3":
+        report()
+    elif val == "quit":
+        exit()
+    else:
+        subprocess.run(["clear"])
+        print("please enter a usable value")
+        menu()
+
+if __name__ == "__main__":
+
+    subprocess.run(["clear"])
+    while True:
+       main()
--- system_report.py
+++ system_report.py
@@ -10,15 +10,74 @@ Date: 10/04/2023
 import subprocess
 import platform
 import re
+import os
+
+
+#Device information
+def device():
+    #linux command hostname / hostname -d
+    #socket.dif
+    hostname = os.popen("hostname | awk -f '.' '{print $1}' ").read()
+    domainname = os.popen("hostname | awk -f '.' '{print $2 $3}'").read()
+    return "\nDevice Information" + "\nHostname:" + "\t" + "\t" + hostname  + "Domain:" + "\t" + "\t" + "\t"+ domainname + "\n"
+#network information
+
+def network():
+    #getting the ip address
+    ip = os.popen("ifconfig | awk 'NR==2{print $2}'").read()
+    gateway = os.popen("ip r | awk 'NR==1{print $3}'").read()
+    subnet = os.popen("ifconfig | awk 'NR==2{print $4}'").read()
+    
+    return "\nNetwork Information" + "\n" + "IP Address: \t\t" + ip + "Gateway:" + "\t" + "\t" + gateway +"\n" + "Network Mask:\t\t" + subnet + "\n"
+
+#os information system, version , kernal version
+def os_info():
+    system = os.popen('uname -o').read()
+    version = os.popen('uname -r').read()
+    kversion = os.popen('uname -v').read()
+
+    return "\nOS Information" +"\nOperating System: \t\t" + system + "Operating Version:\t\t" + version +  "Kernal Version:\t\t\t" + kversion 
+
+#storage information
+def storage():
+    capacity = os.popen("df -h / | awk 'NR==2{print $2}'").read()
+    availible = os.popen("df -h / | awk 'NR==2{print $4}'").read()
+    return "\nStorage Information" +"\n" + "Hard Drive Capacity:\t\t" + capacity + "\n" + "Available Space:\t\t" + availible + "\n"
+
+#process information
+def processor():
+    
+    return "\nProcessor Information" +"\n"
+
+#memory information
+def memory():
+    #free command
+    ram = os.popen("free | awk 'NR==2{print $2}'").read()
+ 
+    ramavailable = os.popen("free | awk 'NR==2{print $3}'").read()
+    
+    return "\nMemory Information" + "\n"+ ram
 
 #writing and creating a file
 def file_edit(text):
    file = open(text, "w")
-   file.write("") 
+   date = os.popen("date").read()
+   file.write("\tSystem Report " + date + "\n") 
+   file.write(device())
+   file.write(network())
+   file.write(os_info())
+   file.write(storage())
+   file.write(processor())
+   file.write(memory())
 
 def main():
     text = "output.txt"
     file_edit(text)
+    output = subprocess.check_output(["cat", text])
+    output = output.decode('utf-8')
+    subprocess.run(["clear"])
+    print(output)
+
 
 if __name__ == "__main__":
     subprocess.run(["clear"])
